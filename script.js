713db5d8da1862600f54e73dd344af45d29795c8
Chukwuemeka Kanu <mekuseo@gmail.com>
2022-11-15 09:10:38 +0100

    menu id lnk

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ const navMenu = document.querySelector("#hamburger-menu");
 toggleBtn.addEventListener("click", () => {
   navMenu.classList.toggle("show-hamburger");
   console.log("click");
+
+  if (hamburgerImg.classList.contains("hamburger")) {
+    hamburgerImg.src = "img/Icon - Cancel.png";
+    hamburgerImg.classList.remove("hamburger");
+  } else {
+    hamburgerImg.src = "img/icons/hamburger.png";
+    hamburgerImg.classList.add("hamburger");
+  }
 });
