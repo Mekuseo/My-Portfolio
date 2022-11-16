dd562ad79384b725473e7886e8ec90a83309c2f5
Chukwuemeka Kanu <mekuseo@gmail.com>
2022-11-16 15:02:35 +0100

    desktop and mobile build - rebuild

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-const toggleBtn = document.querySelector("#hamburger-toggle-btn");
+const hamburger = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('#hamburger-menu');
+const nav = document.querySelector('.nav');
+const navItems = document.querySelectorAll('.nav-item');
 
-const navMenu = document.querySelector("#hamburger-menu");
 
-toggleBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("show-hamburger");
-  console.log("click");
-});
+let showMenu = false;
+
+hamburger.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add('close');
+    navMenu.classList.add('show');
+    navMenu.classList.add('display-menu')
+    nav.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('close');
+    navMenu.classList.remove('show');
+    nav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    showMenu = false;
+  }
+}
